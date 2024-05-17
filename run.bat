7636670006fd211ba435b2a5a35265207ca8d55b
JJRoyale <98667766+JJRoyale@users.noreply.github.com>
2024-05-17 20:25:09 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 @echo off
-ffmpeg.exe -i input.webm -threads:v 4 -sws_flags bicubic -codec:v libvpx  -r:v 25  -b:v 1000k -bufsize 1000k -g 120 -rc_lookahead 16 -profile:v 1 -qmax 51 -qmin 11 -slices 4 -an -vol 0 -b:v 1000k  -aspect 16:9 -b:v 500k -filter:v scale=512:384 output.wii.webm
+ffmpeg.exe -i input.webm -threads:v 4 -sws_flags bicubic -codec:v libvpx  -r:v 25  -b:v 1300k -bufsize 1300k -g 25 -rc_lookahead 16 -p:v 2 -qmax 51 -qmin 11 -slices 4 -an -vol 0 -b:v 1300k  -aspect 16:9 -b:v 1200k -filter:v scale=512:384 output.wii.webm
